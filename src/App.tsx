20f100bd684f6c9ef4121c3fe60188203b6d48bb
M.Nurullah Torun <mntorun@protonmail.com>
2022-08-20 14:32:48 +0300

    deleted unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import "./App.scss";
 import Router from './components/Router/Router'
 import Layout from './components/layout/Layout'
-import logo from './assets/logo.png'
 import routes from "./routes";
 
 function App() {
     return (
         // components will be come here
-        <Layout routes={<Router/>}/>
+        <Layout children={<Router/>} routes=''/>
 
     );
 }
